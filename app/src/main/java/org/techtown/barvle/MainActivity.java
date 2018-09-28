b16cdf7c314534ae70f1002b0ce89debfce0fbfe
Taehoon Bang <peterbang@wustl.edu>
2018-09-28 12:22:50 +0800

    I spelled Barvel wrong, please change the name of the file

--- app/src/main/java/org/techtown/barvle/MainActivity.java
+++ app/src/main/java/org/techtown/barvle/MainActivity.java
@@ -1,13 +1,33 @@
 package org.techtown.barvle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+//BarVle
+    private static final String TAG = MainActivity.class.getSimpleName();
+    Button foodButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        foodButton = (Button) findViewById(R.id.foodButton);
+
+        foodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d(TAG, "Yes, I just clicked my button");
+                //intent go to next page please
+                startActivity(new Intent(MainActivity.this, foodMenuOne.class));
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/org/techtown/barvle/foodMenuOne.java
@@ -0,0 +1,13 @@
+package org.techtown.barvle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class foodMenuOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_menu_one);
+    }
+}
