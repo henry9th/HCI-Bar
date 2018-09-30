07a801929b00ae65255675d4e640cd263e3dc114
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2018-09-30 23:19:44 +0800

    did something on the PaidActivity

--- app/src/main/java/org/techtown/Barvel/PaidActivity.java
+++ app/src/main/java/org/techtown/Barvel/PaidActivity.java
@@ -1,41 +1,25 @@
 package org.techtown.barvel;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 
 public class PaidActivity extends AppCompatActivity {
-    private static final String CHANNEL_ID = "notifier";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paid);
 
-
-        //createNotificationChannel();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                notifyDrinkIsReady();
+                orderIsReady();
             }
         }, 5000);
-
-
     }
-
-    private void notifyDrinkIsReady(){
-        Log.d("log", "notifyDrinkIsReady() called");
+    private void orderIsReady(){
+        Log.d("LOG", "orderIsReady() called");
     }
-
 
 }
