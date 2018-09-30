b7b2ce2101c4034c94f085443a2d806823653218
Taehoon Bang <peterbang@wustl.edu>
2018-09-30 20:44:06 +0800

    Merge branch 'master' of https://github.com/hyunraekim/HCI-Bar
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/org/techtown/Barvel/MainActivity.java
+++ app/src/main/java/org/techtown/Barvel/MainActivity.java
@@ -1,6 +1,11 @@
 package org.techtown.barvel;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,12 +29,41 @@ public class MainActivity extends AppCompatActivity {
         foodButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                    addNotification();
 
-                Log.d(TAG, "Yes, I just clicked my button");
-                //intent go to next page please
-                startActivity(new Intent(MainActivity.this, FoodActivity.class));
-//hi everyone
+                    }
+                }, 6000);
+
             }
         });
     }
+
+    private void addNotification() {
+        // Builds your notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("Your Drink is Ready")
+                .setContentText("Drink Ready!");
+
+        // Creates the intent needed to show the notification
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        // Add as notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+    }
+
+
+
+
 }
+
+
+
+
+
--- app/src/main/java/org/techtown/Barvel/historyScreen.java
+++ app/src/main/java/org/techtown/Barvel/historyScreen.java
@@ -1,17 +1,26 @@
-package org.techtown.Barvel;
+package org.techtown.barvel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class historyScreen extends AppCompatActivity {
+
+    TextView backGroundColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history_screen);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
+        backGroundColor = (TextView) findViewById(R.id.backGroundColor);
+      //  backGroundColor.setBackgroundResource(R.color.colorPrimary);
+
+
+
+//        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+ //       setSupportActionBar(myToolbar);
     }
 }
