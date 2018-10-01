333e5d0b84f08ab9151aff76f9dcb56f7f268479
Taehoon Bang <peterbang@wustl.edu>
2018-10-01 20:57:35 +0800

    peter progress

--- app/src/main/java/org/techtown/Barvel/FavoriteActivity.java
+++ app/src/main/java/org/techtown/Barvel/FavoriteActivity.java
@@ -1,7 +1,10 @@
 package org.techtown.barvel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FavoriteActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class FavoriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fav_screen);
+
+        Button buttona = (Button) findViewById(R.id.buttona);
+
+        buttona.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FavoriteActivity.this, Pop.class));
+            }
+        });
+
     }
 }
