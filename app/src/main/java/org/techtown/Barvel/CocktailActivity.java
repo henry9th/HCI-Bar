2266f2f9939b5222871035e343955f78c8dcd5fa
Hyunrae Kim <hyunrae.kim@gmail.com>
2018-10-02 15:19:20 +0800

    Helen's files

--- app/src/main/java/org/techtown/Barvel/CocktailActivity.java
+++ app/src/main/java/org/techtown/Barvel/CocktailActivity.java
@@ -1,9 +1,9 @@
 package org.techtown.barvel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import org.techtown.barvel.R;
+import android.view.View;
 
 public class CocktailActivity extends AppCompatActivity {
 
@@ -11,5 +11,10 @@ public class CocktailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cocktail);
+    }
+    public void onClickNext(View view) {
+        Intent intent = new Intent(this, MargaritaActivity.class);
+        startActivity(intent);
+
     }
 }
